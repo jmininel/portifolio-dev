b6d0eddb16b0ee8edf4c888da5e28ddf26f65d33
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2026-06-30 17:23:37 +0000

    Add new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import { Header } from "./components/Header";
+import { CarouselProjects } from "./components/CarouselProjects/CarouselProjects";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Hero } from "./components/Hero/Hero";
+import { Stack } from "./components/Stack/Stack"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
-  <Header/>
+    <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black/80">
+      <Header />
+      <main className="px-6 md:px-12 lg:px-24">
+        <Hero />
+        <CarouselProjects />
+      </main>
+      <Stack />
+      <Footer />
     </div>
   );
 }
